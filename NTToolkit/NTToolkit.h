7d3b2c2fd7ac0613a53732faee8207acc7540abf
Nathan Tannar <nathantannar4@gmail.com>
2017-07-30 10:37:46 -0700

    Initial Commit

---
+++ NTToolkit/NTToolkit.h
@@ -0,0 +1,19 @@
+//
+//  NTToolkit.h
+//  NTToolkit
+//
+//  Created by Nathan Tannar on 7/30/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NTToolkit.
+FOUNDATION_EXPORT double NTToolkitVersionNumber;
+
+//! Project version string for NTToolkit.
+FOUNDATION_EXPORT const unsigned char NTToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTToolkit/PublicHeader.h>
+
+
